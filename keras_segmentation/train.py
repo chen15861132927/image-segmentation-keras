5fff00fc028a636fab208049cefe2ee3af757d87
WeiChen <69610388+chenweiSeagate@users.noreply.github.com>
2022-06-15 09:28:25 +0800

    ca load acc

--- keras_segmentation/train.py
+++ keras_segmentation/train.py
@@ -1,6 +1,8 @@
 import json
 import os
 
+from numpy import true_divide
+
 from .data_utils.data_loader import image_segmentation_generator, \
     verify_segmentation_dataset
 import six
@@ -10,12 +12,21 @@ import tensorflow as tf
 import glob
 import sys
 
+def get_epoch_number_from_path(path):
+    return os.path.basename(path).strip(".").split(".")[0]
+
 def find_latest_checkpoint(checkpoints_path, fail_safe=True):
 
     # This is legacy code, there should always be a "checkpoint" file in your directory
 
-    def get_epoch_number_from_path(path):
-        return path.replace(checkpoints_path, "").strip(".")
+    def check_all_number_from_path(path):
+        splits= path.replace(checkpoints_path, "").strip(".").split(".")
+        isallnumber=True
+        for item in splits:
+            if not item.strip(".").isdigit():
+                isallnumber=False
+                break
+        return isallnumber
 
     # Get all matching files
     all_checkpoint_files = glob.glob(checkpoints_path + ".*")
@@ -24,9 +35,8 @@ def find_latest_checkpoint(checkpoints_path, fail_safe=True):
     all_checkpoint_files = [ff.replace(".index", "") for ff in
                             all_checkpoint_files]  # to make it work for newer versions of keras
     # Filter out entries where the epoc_number part is pure number
-    all_checkpoint_files = list(filter(lambda f: get_epoch_number_from_path(f)
-                                       .isdigit(), all_checkpoint_files))
-    if not len(all_checkpoint_files):
+    filterall_checkpoint_files = list(filter(lambda f: check_all_number_from_path(f), all_checkpoint_files))
+    if not len(filterall_checkpoint_files):
         # The glob list is empty, don't have a checkpoints_path
         if not fail_safe:
             raise ValueError("Checkpoint path {0} invalid"
@@ -35,7 +45,7 @@ def find_latest_checkpoint(checkpoints_path, fail_safe=True):
             return None
 
     # Find the checkpoint file with the maximum epoch
-    latest_epoch_checkpoint = max(all_checkpoint_files,
+    latest_epoch_checkpoint = max(filterall_checkpoint_files,
                                   key=lambda f:
                                   int(get_epoch_number_from_path(f)))
 
@@ -150,7 +160,7 @@ def train(model,
                   latest_checkpoint)
             model.load_weights(latest_checkpoint)
 
-            initial_epoch = int(latest_checkpoint.split('.')[-1])
+            initial_epoch = int(get_epoch_number_from_path(latest_checkpoint))
 
     if verify_dataset:
         print("Verifying training dataset")
