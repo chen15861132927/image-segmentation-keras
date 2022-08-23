4a01f9aa94411d309897bb00ad5510e3d854e917
WeiChen <69610388+chenweiSeagate@users.noreply.github.com>
2022-08-23 10:39:15 +0800

    Update predict.py

--- keras_segmentation/predict.py
+++ keras_segmentation/predict.py
@@ -127,7 +127,47 @@ def visualize_segmentation(seg_arr, inp_img=None, n_classes=None,
         seg_img = concat_lenends(seg_img, legend_img)
 
     return seg_img
+    
+def predict_segmentation(model=None, inp=None, out_fname=None,
+            checkpoints_path=None, overlay_img=False,
+            class_names=None, show_legends=False, colors=class_colors,
+            prediction_width=None, prediction_height=None,
+            read_image_type=1):
 
+    if model is None and (checkpoints_path is not None):
+        model = model_from_checkpoint_path(checkpoints_path)
+
+    assert (inp is not None)
+    assert ((type(inp) is np.ndarray) or isinstance(inp, six.string_types)),\
+        "Input should be the CV image or the input file name"
+
+    if isinstance(inp, six.string_types):
+        inp = cv2.imread(inp, read_image_type)
+
+    assert (len(inp.shape) == 3 or len(inp.shape) == 1 or len(inp.shape) == 4), "Image should be h,w,3 "
+
+    output_width = model.output_width
+    output_height = model.output_height
+    input_width = model.input_width
+    input_height = model.input_height
+    n_classes = model.n_classes
+
+    x = get_image_array(inp, input_width, input_height,
+                        ordering=IMAGE_ORDERING)
+    pr = model.predict(np.array([x]))[0]
+    pr = pr.reshape((output_height,  output_width, n_classes)).argmax(axis=2)
+
+    seg_img = visualize_segmentation(pr, inp, n_classes=n_classes,
+                                     colors=colors, overlay_img=overlay_img,
+                                     show_legends=show_legends,
+                                     class_names=class_names,
+                                     prediction_width=prediction_width,
+                                     prediction_height=prediction_height)
+
+    if out_fname is not None:
+        cv2.imwrite(out_fname, seg_img)
+
+    return pr
 
 def predict(model=None, inp=None, out_fname=None,
             checkpoints_path=None, overlay_img=False,
